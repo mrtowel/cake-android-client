f74aecb6cbb3195b2752e185510109dd9277f671
Przemyslaw Bis <przemyslaw.bis@gmail.com>
2017-06-17 11:24:49 +0200

    Add NetworkResponse

---
+++ app/src/main/java/com/waracle/androidtest/NetworkResponse.java
@@ -0,0 +1,28 @@
+package com.waracle.androidtest;
+
+
+public final class NetworkResponse {
+
+    private int responseCode;
+    private byte[] body;
+    private String contentType;
+
+    public NetworkResponse(final int responseCode, final byte[] body, final String contentType) {
+        this.responseCode = responseCode;
+        this.body = body;
+        this.contentType = contentType;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+}
